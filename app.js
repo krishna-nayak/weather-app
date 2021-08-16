268f6617cc273a91a919d175039724c4f0cecc24
Krishna Nayak <nayak100krishna@gmail.com>
2021-08-16 19:08:03 +0530

    API connection with template

--- app.js
+++ app.js
@@ -5,26 +5,32 @@ const fetch = require("node-fetch");
 
 const app = express();
 
+async function getData(cityName) {
+  const API_key = "06e37de5c4e8acab1d3cbc938a401688";
+  const unit = "metric";
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_key}&units=${unit}`;
+  console.log(url);
+  // Data fetch
+  const weather = await fetch(url);
+  const weatherJSON = await weather.json();
+  return weatherJSON;
+}
+
+let cityName = "delhi";
+
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (request, response) => {
-  response.render("index");
+app.get("/", async (request, response) => {
+  let data = await getData(cityName);
+  response.render("index", { cityName: data.name, description: data.weather[0].description, temp: data.main.temp });
 });
 
 app.post("/", async function (request, response) {
-  const cityName = request.body.location;
-
-  const API_key = "06e37de5c4e8acab1d3cbc938a401688";
-
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_key}`;
-
-  // Data fetch
-  const weather = await fetch(url);
-  const weatherData = await weather.json();
-
-  console.log(weatherData);
+  let previousData = cityName;
+  cityName = request.body.location != "" ? request.body.location : previousData;
 
   response.redirect("/");
 });
