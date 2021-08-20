31142a941a1deaa70ce3c804fb9527fa0ad283e2
Krishna Nayak <nayak100krishna@gmail.com>
2021-08-20 21:54:13 +0530

    tailwindcss --Optimizing-production

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 const plugin = require("tailwindcss/plugin");
 module.exports = {
-  purge: [],
+  purge: ["./views/**/*.ejs"],
   darkMode: false, // or 'media' or 'class'
   theme: {},
   variants: {
